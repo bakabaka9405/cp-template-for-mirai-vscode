09d20a3995d3631f9e69ffc47c2839a46b2e6f12
bakabaka9405 <2677423642@qq.com>
2024-04-24 22:08:29 +0800

    初版

---
+++ include/test.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+void foo() {
+	std::cout << "test" << std::endl;
+}
---
+++ src/test.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	vector v{ 1, 2, 3 };
+	for (auto [i, j] : views::enumerate(v)) {
+		
+	}
+}	
